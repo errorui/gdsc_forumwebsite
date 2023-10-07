c845e4dd97232a6901102af2516f59ac8fc3404a
errorui <139210987+errorui@users.noreply.github.com>
2023-10-07 11:26:04 +0530

    Update banner.jsx

--- src/app/components/banner.jsx
+++ src/app/components/banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
             <div className="bannercontent">
                 <h1 className='banner-text'> welcome to W-help</h1>
                 <p>ask your  coding related doubts and <br />get instant  <span>answer</span></p>
-                <p>from proffessionals and experienced developers</p>
+                <p>from professional and experienced developers</p>
                 <div className="btn">
                 <Link  className="btn-text"href="doubt">ask doubt</Link>
                 
